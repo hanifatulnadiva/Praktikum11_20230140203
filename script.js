8e51a9bc8277690d1be8360da23854aa54c976b5
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-05-30 14:19:58 +0700

    update

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function kirimData() {
     var name = document.getElementById("nama").value;
     var nim = document.getElementById("nim").value;
+    var angkatan = document.getElementById("angkatan").value;
+    var tanggal = document.getElementById("tanggal").value;
     var peminatan = document.querySelector("input[name=peminatan]:checked").value;
     var alamat = document.getElementById("alamat").value;
 
     alert(
         "Nama: " + name +
         "\nNIM: " + nim +
+        "\nAngkatan: " + angkatan + 
+        "\nTanggal: " + tanggal +
         "\nPeminatan: " + peminatan +
-        "\nAlamat: " + alamat
+        "\nAlamat: " + alamat 
     );
 }
